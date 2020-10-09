9c02f8605bfaca22a5976f7f4fbc5bc5c716101a
Ke Ning <ke.ning.16@ucl.ac.uk>
2020-10-09 18:16:34 +0800

    ##Intial submission. Cache the inverse of an invertible matrix

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,30 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- matrix(NA,nrow=nrow(x),ncol=ncol(x))
+  set <- function(y) {
+    x <<- y
+    inv <<- matrix(NA,nrow=nrow(x),ncol=ncol(x))
+  }
+  get <- function() x
+  setinv <- function(inversem) inv <<- inversem
+  getinv <- function() inv
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinv()
+  if(all(!is.na(inv))) {
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data)
+  inv<-x$setinv(inv)
+  inv## Return a matrix that is the inverse of 'x'
 }
